35f9d33bd1a8c87a7bfaf2ea62a5b5c42acf2fd4
TawanaShava <94750339+TawanaShava@users.noreply.github.com>
2022-04-17 21:30:28 +0200

    Update Person.java

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -33,7 +33,7 @@ public class Person {
     }
 
 
-    class Staff extends com.company.Person {
+    class Staff extends com.company.Person implements Salary{
         String education , Position;
         void Initialize1(){
             Initialize();
@@ -49,12 +49,47 @@ public class Person {
             System.out.println("Position: "+ Position);
         }
 
+
+        @Override
+        public void salaryToPayShava56415(int hoursShava56415, int RateShava56415) {
+
+        }
+
+        @Override
+        public int salaryForOvertime(int extrahoursShava56415, int rateShava56415) {
+            return Salary.super.salaryForOvertime(extrahoursShava56415, rateShava56415);
+        }
     }
+
+     interface Salary{
+     void salaryToPayShava56415(int hoursShava56415, int RateShava56415);
+
+
+
+    default int salaryForOvertime(int extrahoursShava56415, int rateShava56415){
+        int salaryOverTime=extrahoursShava56415 * rateShava56415;
+        return salaryOverTime;
+
+    }
+
+    static double bonusCheckShava56415( int yearsOfExpirience){
+        double bonusShava56415=0;
+        if (yearsOfExpirience>3) bonusShava56415=100;
+        else bonusShava56415=0;
+        return bonusShava56415;
+    };
+
+
+    }
+
 
 class Main {
     public static void main(String[] args) {
         Staff employee = new Staff();
         employee.Initialize1();
         employee.Print1();
+        employee.salaryToPayShava56415(5,9);
+        Salary.bonusCheckShava56415(7);
     }
 }
+
